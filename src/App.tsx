bba05d53801e00fdee18bac73e7cba5b1a23103a
ahmad <ahmad.b1995@gmail.com>
2022-08-22 15:18:39 +0430

    font awesome added to pdfmake

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,99 @@
 import React from "react";
-import "./App.css";
+import "./assets/styles/App.css";
 import Section1 from "./components/section1/section1";
-import pdfmake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
-pdfmake.vfs = pdfFonts.pdfMake.vfs;
+import { Resume } from "./models/resume";
+import pdfMake from "pdfmake/build/pdfmake";
+import { vfs } from "./assets/fonts/vfs_fonts.js";
+// import * as pdfFonts from "./assets/fonts/vfs_fonts";
+// pdfMake.vfs = pdfFonts.pdfMake.vfs;
+// import { useEffect, useRef, useState } from "react";
+pdfMake.vfs = vfs;
 
 class App extends React.Component {
-  data: { [key: string]: string } = {};
+  resume: Resume;
+
+  constructor(props: any) {
+    super(props);
+    this.resume = new Resume();
+    pdfMake.fonts = {
+      Roboto: {
+        normal: "Roboto-Regular.ttf",
+        bold: "Roboto-Medium.ttf",
+        italics: "Roboto-Italic.ttf",
+        bolditalics: "Roboto-MediumItalic.ttf",
+      },
+      Fa: {
+        normal: "fa.ttf",
+      },
+    };
+  }
 
   handleClick = (key: string, value: string) => {
-    this.data[key] = value;
-    this.setState(this.data);
+    this.resume[key] = value;
+    // this.setState(this.data);
   };
 
   generatePdf = () => {
-    var dd = {
+    const documentDefinition = this.getDocumentDefinition();
+    pdfMake.createPdf(documentDefinition).open();
+  };
+
+  getDocumentDefinition(): any {
+    return {
       content: [
         {
-          layout: "lightHorizontalLines",
-          table: {
-            headerRows: 1,
-            widths: ["*", "auto", 100, "*"],
-
-            body: [
-              [
-                { text: this.data.name, bold: true },
-                { text: "Second", bold: true },
-                { text: "Third", bold: true },
-                { text: "The last one", bold: true },
-              ],
-              ["Value 1", "Value 2", "Value 3", "Value 4"],
-              ["Val 2", "Val 2", "Val 3", "Val 4"],
+          columns: [
+            [
+              {
+                text: this.resume.name,
+                style: "name",
+              },
+              {
+                text: this.resume.website,
+                style: "info",
+              },
+              {
+                text: this.resume.linkedIn,
+                style: "info",
+              },
+              {
+                text: this.resume.github,
+                style: "info",
+              },
+              { text: "\ue800", style: "icon" },
+              {
+                text: this.resume.email,
+                style: "info",
+              },
+              {
+                text: this.resume.phoneNumber,
+                style: "info",
+              },
             ],
-          },
+          ],
         },
       ],
+      pageMargins: 10,
+      styles: {
+        name: {
+          fontSize: 14,
+          bold: true,
+          color: "#3b4147",
+          marginBottom: 15,
+        },
+        info: {
+          fontSize: 11,
+          color: "#5b6e80",
+          marginBottom: 5,
+        },
+        icon: {
+          font: "Fa",
+          fontSize: 200,
+          color: "black",
+        },
+      },
     };
-    pdfmake.createPdf(dd).download();
-  };
+  }
 
   render() {
     return (
---
+++ src/models/resume.ts
@@ -0,0 +1,13 @@
+export class Resume {
+    [key: string]: string | undefined;
+    name?: string
+    country?: string
+    website?: string
+    linkedIn?: string
+    github?: string
+    email?: string
+    phoneNumber?: string
+}
+
+
+
